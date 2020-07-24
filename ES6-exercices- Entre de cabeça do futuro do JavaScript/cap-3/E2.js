d5327296184f54c77c41f22e00785b5e217719e4
GustavoFortti <53984056+GustavoFortti@users.noreply.github.com>
2020-07-23 21:15:21 -0300

    Add files via upload

---
+++ ES6-exercices- Entre de cabeça do futuro do JavaScript/cap-3/E2.js
@@ -0,0 +1,18 @@
+/**/
+/*
+
+Exercício 2 - Quero o dobro
+Utilizando o método map, escreva o método dobrar que 
+recebe um array de números inteiros e retorna um array 
+com todos os valores do array original dobrados.
+
+Exemplo: dobrar([1,2,3]) → [2,4,6]
+
+*/
+
+
+var numeros = [0, 1, 2, 3, 4, 5];
+
+const dobrar = numeros.map(El => El * 2);
+console.log(numeros);
+console.log(dobrar);
---
+++ ES6-exercices- Entre de cabeça do futuro do JavaScript/cap-3/E3.js
@@ -0,0 +1,17 @@
+/**/
+/*
+Exercício 3 - NÃO ESTOU BRAVO
+Escreva o método caps que recebe um array de strings 
+e retorna um outro array com todas as strings do 
+array original em CAIXA ALTA.
+
+Exemplo: caps(['oi', 'tudo', 'bem?']) → ['OI', 'TUDO', 'BEM?']
+*/
+
+var arr = ['oi', 'tudo', 'bem?'];
+
+const caps = arr.map(El => {
+    return El.toUpperCase();
+})
+
+console.log(caps);
